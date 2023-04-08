307c10bbf6f25930b592c9202cf83af183590f2b
Azad-sl <85327306+Azad-sl@users.noreply.github.com>
2023-04-08 18:09:20 +0800

    Update zh-CN.ts

--- src/locales/zh-CN.ts
+++ src/locales/zh-CN.ts
@@ -25,7 +25,7 @@ export default {
     success: '操作成功',
     failed: '操作失败',
     verify: '验证',
-    unauthorizedTips: '未经授权，请先进行验证。',
+    unauthorizedTips: '未经授权，Q群 340825795 公告处领取授权码。',
   },
   chat: {
     placeholder: '来说点什么吧...（Shift + Enter = 换行）',
